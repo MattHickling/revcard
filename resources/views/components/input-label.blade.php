9b14dec23db31cb3f32e2906d54798259920955d
Matt Hickling <m.hickling@hotmail.co.uk>
2025-06-22 19:48:35 +0100

    input filed styling

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,7 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-900']) }}>
+<label {{ $attributes->merge([
+    'style' => 'display: block; font-weight: 500; font-size: 0.875rem; color: #1f2937; background-color: #f9fafb; padding: 6px 12px; border-radius: 4px;'
+]) }}>
     {{ $value ?? $slot }}
 </label>
